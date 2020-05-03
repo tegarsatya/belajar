1838ac87fa6b943cba6501e70fa51311c8ddc35a
tegarsatya <59832647+tegarsatya@users.noreply.github.com>
2020-05-02 22:32:29 -1200

    first commit

--- resources/views/pages/space/index.blade.php
+++ resources/views/pages/space/index.blade.php
@@ -6,20 +6,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+             <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+              </div>
+              @endif
 
-                    @foreach($spaces as $space)
+               @foreach($spaces as $space)
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">
-                                {{ $space->title }}
+                            {{ $space->title }}
                                 <form action="#">
                                     @csrf @method('DELETE')
-                                    <button type="submit" class="btn btn-sm btn-danger">Delete</button>
-                                    <a href="#" class="btn btn-sm btn-info float-right">Edit</a>
+                                    <button type="submit" class="btn btn-sm btn-danger float-right">Delete</button>
+                                    <a href="#" class="btn btn-sm btn-info float-right text-white">Edit</a>
                                 </form>
                             </h5>
                             <h6 class="card-subtitle">{{ $space->address }}</h6>
@@ -27,11 +27,11 @@
                             <a href="#" class="card-link">Direction</a>
                         </div>
                     </div>
-                    @endforeach
-            </div>
-            <div class="row justify-content-center">
-                {{ $spaces->links() }}
-            </div>
+                @endforeach
+           </div>
     </div>
+    <div class="row justify-content-center">
+            {{ $spaces->links() }}
+     </div>
 </div>
 @endsection
